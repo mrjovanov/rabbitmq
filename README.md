2604fe8b053021ced5fa114069d21eae11fe7d16
Mioljub Jovanovic <mjovanov@ax3.mionet.local>
2023-12-22 14:20:15 +0100

    README updejtovan

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # rabbitmq projekat za Univerzitet u Kragujevcu
-# predmet: Integracija aplikacija preko sistema za prenos poruka
-# autor: Milan Čabarkapa
+- predmet: Integracija aplikacija preko sistema za prenos poruka
+- autor: Milan Čabarkapa
+
+Uputstvo:
+cd rabbitmq/java
+curl -O https://repo1.maven.org/maven2/com/rabbitmq/amqp-client/5.16.0/amqp-client-5.16.0.jar
+curl -O https://repo1.maven.org/maven2/org/slf4j/slf4j-api/1.7.36/slf4j-api-1.7.36.jar
+curl -O https://repo1.maven.org/maven2/org/slf4j/slf4j-simple/1.7.36/slf4j-simple-1.7.36.jar
---
+++ java/Recv.java
@@ -0,0 +1,26 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+import java.nio.charset.StandardCharsets;
+
+public class Recv {
+
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Cekamo poruku iz queue: " + QUEUE_NAME + ". Za izlaz pritisni CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            System.out.println(" [x] Primljena poruka '" + message + "'");
+        };
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+    }
+}
